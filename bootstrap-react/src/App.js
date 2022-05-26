6c5c94abf886ea96267944712cc0674c3808af83
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-05-26 17:34:03 +0700

    Cards dan Carousel

--- bootstrap-react/src/App.js
+++ bootstrap-react/src/App.js
@@ -8,7 +8,9 @@ import {
   Button,
   Breadcrumb,
   ButtonGroup,
-  ButtonToolbar
+  ButtonToolbar,
+  Card,
+  Carousel,
 } from "react-bootstrap";
 
 function App() {
@@ -19,6 +21,42 @@ function App() {
           <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
           <Breadcrumb.Item href="/halaman">Info</Breadcrumb.Item>
         </Breadcrumb>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              alt="gambar1"
+              src=" https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel1.jpeg "
+            />
+            <Carousel.Caption>
+              <h3>Tiket Pesawat</h3>
+              <p>Dapatkan Promo</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              alt="gambar1"
+              src=" https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel2.png"
+            />
+            <Carousel.Caption>
+              <h3>Tiket Pesawat</h3>
+              <p>Dapatkan Promo</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              alt="gambar1"
+              src=" https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/travel3.jpeg"
+            />
+            <Carousel.Caption>
+              <h3>Tiket Pesawat</h3>
+              <p>Dapatkan Promo</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+        <br/>
         <Row>
           <Col>1 of 2</Col>
           <Col>
@@ -52,6 +90,24 @@ function App() {
           <Col>
             <Button variant="primary">tes</Button>
           </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+            <Card style={{ width: "300px" }}>
+              <Card.Img
+                variant="top"
+                src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png "
+              />
+              <Card.Body>
+                <Card.Title>Sate</Card.Title>
+                <Card.Text>Sate adalaha makanan</Card.Text>
+                <Button variant="primary">tes</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col></Col>
+          <Col></Col>
         </Row>
       </Container>
     </div>
